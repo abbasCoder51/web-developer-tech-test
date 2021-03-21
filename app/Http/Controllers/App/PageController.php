1addf6b57d40fb69a69e36cf61005c517cfbc67b
abbascoder51@gmail.com <Time2code91>
2021-03-21 22:02:12 +0000

    - Added functionality for ajax for filtering featured talents, - Styling added to featured talents page

--- app/Http/Controllers/App/PageController.php
+++ app/Http/Controllers/App/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\App;
 
 use App\Http\Controllers\Controller;
 use App\Models\Admin\FeaturedTalent;
+use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
@@ -12,16 +13,42 @@ class PageController extends Controller
         return view('app.index');
     }
 
-    public function talent()
+    public function talent(Request $request)
     {
-        $featuredTalents = FeaturedTalent::query()->get();
-        $locations = $featuredTalents->unique('location')->pluck('location');
-        $jobCategories = $featuredTalents->unique('job_category')->pluck('job_category');
-        $employmentTypes = $featuredTalents->unique('employment_type')->pluck('employment_type');
-        $skills = $featuredTalents->unique('skill')->pluck('skill');
+        $featuredTalents = FeaturedTalent::query();
+        $locations = $featuredTalents->get()->unique('location')->pluck('location');
+        $jobCategories = $featuredTalents->get()->unique('job_category')->pluck('job_category');
+        $employmentTypes = $featuredTalents->get()->unique('employment_type')->pluck('employment_type');
+        $skills = $featuredTalents->get()->unique('skill')->pluck('skill');
+
+        if($request->ajax()) {
+            if ($request->get('location')) {
+                $featuredTalents = $featuredTalents->where('location', $request->get('location'));
+            }
+
+            if ($request->get('job_category')) {
+                $featuredTalents = $featuredTalents->where('job_category', $request->get('job_category'));
+            }
+
+            if ($request->get('employment_type')) {
+                $featuredTalents = $featuredTalents->where('employment_type', $request->get('employment_type'));
+            }
+
+            if ($request->get('skill')) {
+                $featuredTalents = $featuredTalents->where('skill', $request->get('skill'));
+            }
+
+            if ($request->get('years_experience')) {
+                $featuredTalents = $featuredTalents->where('years_experience', '>=', $request->get('years_experience'));
+            }
+
+            return [
+                'data' => $featuredTalents->get()->toArray()
+            ];
+        }
 
         return view('app.talent')
-            ->with('featuredTalents', $featuredTalents)
+            ->with('featuredTalents', $featuredTalents->get())
             ->with('locations', $locations)
             ->with('jobCategories', $jobCategories)
             ->with('employmentTypes', $employmentTypes)
--- resources/views/app/talent.blade.php
+++ resources/views/app/talent.blade.php
@@ -28,61 +28,66 @@
                 <div class="col-sm-10 offset-sm-1">
                     <h1 class="text-center text-uppercase text-uppercase mb-5">Featured Talent</h1>
                     <div class="row">
-                        <div class="col-sm-2">
+                        <div class="col-md-12 col-lg-2 search-filter-title d-flex justify-content-center pb-3">
                             Search Filter
                         </div>
-                        <div class="col-sm-10">
+                        <div class="col-md-12 col-lg-10 featured-talent-search-container">
                             <form id="featured-talent-form" class="featured-talent-form" action="">
                                 <div class="row">
-                                    <div class="col-sm-2">
+                                    <div class="col-2">
+                                        <label for="location">Location</label>
                                         <div class="form-group">
                                             <select class="form-control" name="location">
-                                                <option value="">Location</option>
+                                                <option value="">All</option>
                                                 @foreach($locations as $name)
                                                     <option value="{{ $name }}">{{ $name }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="col-sm-2">
+                                    <div class="col-2">
                                         <div class="form-group">
+                                            <label for="role">Role</label>
                                             <select class="form-control" name="job_category">
-                                                <option value="">Role</option>
+                                                <option value="">All</option>
                                                 @foreach($jobCategories as $name)
                                                     <option value="{{ $name }}">{{ $name }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="col-sm-3">
+                                    <div class="col-3">
                                         <div class="form-group">
+                                            <label for="employment_type">Contract</label>
                                             <select class="form-control" name="employment_type">
-                                                <option value="">Employment Type</option>
+                                                <option value="">All</option>
                                                 @foreach($employmentTypes as $name)
                                                     <option value="{{ $name }}">{{ $name }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="col-sm-2">
+                                    <div class="col-2">
                                         <div class="form-group">
+                                            <label for="skill">Skill</label>
                                             <select class="form-control" name="skill">
-                                                <option value="">Skill</option>
+                                                <option value="">All</option>
                                                 @foreach($skills as $name)
                                                     <option value="{{ $name }}">{{ $name }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="col-sm-3">
+                                    <div class="col-3">
                                         <div class="form-group">
+                                            <label for="years_experience">Experience</label>
                                             <select class="form-control" name="years_experience">
-                                                <option value="">Years Experience</option>
-                                                <option value="1">1+</option>
-                                                <option value="1">2+</option>
-                                                <option value="1">3+</option>
-                                                <option value="1">4+</option>
-                                                <option value="1">5+</option>
+                                                <option value="">All</option>
+                                                <option value="1">1+ Years</option>
+                                                <option value="2">2+ Years</option>
+                                                <option value="3">3+ Years</option>
+                                                <option value="4">4+ Years</option>
+                                                <option value="5">5+ Years</option>
                                             </select>
                                         </div>
                                     </div>
@@ -93,7 +98,7 @@
                 </div>
             </div>
             <div class="row pt-5">
-                <div class="col-sm-10 offset-sm-1">
+                <div id="featured-talent-container" class="col-sm-10 offset-sm-1">
                     @forelse($featuredTalents as $talent)
                         <div class="featured-talent-item">
                             <div class="featured-talent-image">
@@ -108,11 +113,55 @@
                             </div>
                         </div>
                     @empty
-                        <div class="text-center">No Results</div>
+                        <div class="text-center">No Results Found</div>
                     @endforelse
                 </div>
             </div>
         </div>
     </div>
     <!-- End Featured Talent -->
+
+    <!-- Begin Featured Talent Template -->
+    <div class="d-none">
+        <div id="featured-talent-item-template" class="featured-talent-item">
+            <div class="featured-talent-image">
+                <img src="">
+            </div>
+            <div class="featured-talent-content">
+                <h1 class="featured-talent-title"></h1>
+                <p class="featured-talent-description"></p>
+                <a class="featured-talent-url-link" href="">Read More ></a>
+            </div>
+        </div>
+    </div>
+    <!-- End Featured Talent Template -->
 @endsection
+
+@push('scripts_bottom')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#featured-talent-form").on("change", function() {
+                $.ajax({
+                    url: "{{ route('talent') }}",
+                    data: $(this).serialize(),
+                    success: function (result) {
+                        var featuredTalentContainer = $("#featured-talent-container");
+                        featuredTalentContainer.empty();
+                        if(result.data.length > 0) {
+                            result.data.forEach(function(talent) {
+                                var featuredTalentItem = $("#featured-talent-item-template").clone().removeAttr("id");
+                                featuredTalentItem.find(".featured-talent-image").find("img").attr("src", talent["image"]);
+                                featuredTalentItem.find(".featured-talent-content .featured-talent-title").html(talent["name"]);
+                                featuredTalentItem.find(".featured-talent-content .featured-talent-description").html(talent["description"]);
+                                featuredTalentItem.find(".featured-talent-content .featured-talent-url-link").attr("href", talent["url_link"]);
+                                featuredTalentContainer.append(featuredTalentItem);
+                            });
+                        } else {
+                            featuredTalentContainer.html("<div class='text-center'>No Results Found</div>");
+                        }
+                    }
+                });
+            });
+        });
+    </script>
+@endpush
