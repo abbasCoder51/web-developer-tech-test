456528f4f4731b41c1aff0fe21612b74a9791a86
abbascoder51@gmail.com <Time2code91>
2021-03-22 08:47:30 +0000

    - Updated structure of footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,13 +1,13 @@
 <footer class="pt-5 pb-5">
     <div class="container">
         <div class="row">
-            <div class="col-sm-2">
+            <div class="col-md-2">
                 <img class="footer-logo" src="{{ asset('images/logo-red-1.png') }}" alt="Logo">
             </div>
-            <div class="col-sm-4">
+            <div class="col-md-4 mt-4">
                 <h3>Links</h3>
                 <div class="row">
-                    <div class="col-sm-6">
+                    <div class="col-6">
                         <ul>
                             <li><a href="#">Link 1</a></li>
                             <li><a href="#">Link 2</a></li>
@@ -15,7 +15,7 @@
                             <li><a href="#">Link 4</a></li>
                         </ul>
                     </div>
-                    <div class="col-sm-6">
+                    <div class="col-6">
                         <ul>
                             <li><a href="#">Link 1</a></li>
                             <li><a href="#">Link 2</a></li>
@@ -25,14 +25,14 @@
                     </div>
                 </div>
             </div>
-            <div class="col-sm-2">
+            <div class="col-md-2 mt-4">
                 <h3>Address</h3>
                 Unit 14 Lorem Ipsum<br>
                 Lorem Ipsum Avenue<br>
                 Lorem Ipsum Park<br>
                 Lorem Ipsum, LI7 1PS
             </div>
-            <div class="col-sm-4">
+            <div class="col-md-4 mt-4">
                 <b>Manchester</b><br>
                 0116 232252<br>
                 <b>London</b><br>
