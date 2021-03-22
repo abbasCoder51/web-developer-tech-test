510e8a5f0883f26304c242e6e0658ff8698be8f0
abbascoder51@gmail.com <Time2code91>
2021-03-22 08:54:55 +0000

    - Added section functionality

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -532,6 +532,7 @@ class ComposerStaticInitde15032ad1c15a6b807935848e528f4c
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Models\\Admin\\FeaturedTalent' => __DIR__ . '/../..' . '/app/Models/Admin/FeaturedTalent.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
