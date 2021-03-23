f263a084fe6a2e81a080abfd8f6f4f3001e34227
abbascoder51@gmail.com <Time2code91>
2021-03-23 23:06:25 +0000

    - Added prefix in front of stylesheet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base')
 
 @push("styles")
-    <link rel="stylesheet" href="{{ asset("css/style.css") }}">
+    <link rel="stylesheet" href="{{ asset("css/style.css") }}?v=1">
 @endpush
 
 @section('body')
